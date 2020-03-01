da0cf7ca7126d446f827fa73089925579d36903d
Nandun <nandun.gunawardhana@gmail.com>
2020-03-01 17:22:03 -0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:nvfs-test.database.windows.net,1433; Database = Testing", "Nandun", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Nandun", "pwd" => "{your_password_here}", "Database" => "Testing", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:nvfs-test.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
+</body>
+</html>
