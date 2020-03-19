a12c2f9f301f45ead9f5af4d928ee11cd3e47486
FrederickOlson <61254701+FrederickOlson@users.noreply.github.com>
2020-03-19 16:34:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   // Preloader
   $(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
+      $('#preloader').delay(1200).fadeOut('slow', function () {
         $(this).remove();
       });
     }
